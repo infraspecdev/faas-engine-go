27af89d02bc36067f2f1c1f5fa5071feb56e6c36
sreyasppradeep1455 <sreyas.int26@infraspec.dev>
2026-02-24 17:27:27 +0530

    refactor(deploycli): managed the errors in deploy cli

--- cmd/cli/cmd/deploy.go
+++ cmd/cli/cmd/deploy.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"faas-engine-go/internal/buildcontext"
+	"fmt"
 	"log/slog"
 	"path/filepath"
 
@@ -15,40 +16,41 @@ import (
 var deployCmd = &cobra.Command{
 	Use:   "deploy",
 	Short: "deploy a function in the runtime manager",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Args: cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
+	Long: `Example usage:
+lambda deploy --file ./my-function --function-name my-function
+	`,
+	RunE: func(cmd *cobra.Command, args []string) error {
 		if filePath == "" {
-			panic("file path is required")
+			return fmt.Errorf("file path is required")
 		}
 
 		if functionName == "" {
-			panic("function name is required")
+			return fmt.Errorf("function name is required")
 		}
 
 		abspath, err := filepath.Abs(filePath)
 		if err != nil {
-			panic("failed to get absolute path: " + err.Error())
+			return fmt.Errorf("failed to get absolute path: %w", err)
 		}
 		//create a tar stream of the function directory
 		tarstream, err := buildcontext.CreateTarStream(abspath)
 		if err != nil {
-			panic("failed to create tar stream: " + err.Error())
+			return fmt.Errorf("failed to create tar stream: %w", err)
 		}
 
 		//send the tarstream to the server
 		Response, err := buildcontext.SendTarStream(tarstream, "http://localhost:8080/functions", functionName)
 		if err != nil {
-			slog.Info("failed to send tar stream", "error", err)
-			return
+			slog.Info("failed to send tar stream",
+				"error", err,
+			)
+			return fmt.Errorf("failed to send tar stream: %w", err)
 		}
 
-		slog.Info("response from server:", "Message", Response)
+		slog.Info("response from server:",
+			"Message", Response,
+		)
+		return nil
 	},
 }
 
